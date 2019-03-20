884d3ec5979cd074fcf2f9fadf2c75397cc7a64c
PatrickTrentin88 <patrick.trentin.88@gmail.com>
2019-03-20 15:29:09 +0100

    simple_omt.py

---
+++ unit-tests/simple_omt.py
@@ -0,0 +1,87 @@
+"""
+Simple OMT problem unit-test.
+"""
+
+from wrapper import * # pylint: disable=unused-wildcard-import,wildcard-import
+
+###
+### DATA
+###
+
+OPTIONS = {
+    "opt.priority"     : "box",
+    "model_generation" : "true",
+}
+
+DECLS = {
+    "bool" : ("B1", ),              # (name, ...)
+    "int"  : ("x1", "x2", "cost"),  # (name, ...)
+    "rational" : (),                # (name, ...)
+    "bv" : (),                      # ((name, width), ... )
+    "fp" : ()                       # ((name, ebits, sbits), ... )
+}
+
+HARD = [
+    "(<= 0 x1)", "(<= x1 10)",
+    "(<= 0 x2)", "(<= x2 10)",
+    "(not (= x1 x2))", "B1",
+    "(=> B1 (< x1 x2))",
+    "(=> (not B1) (< x2 x1))",
+    "(= cost (+ x1 x2))"
+]
+
+SOFT = {}
+
+###
+### SIMPLE OMT PROBLEM UNIT-TEST
+###
+
+with create_config(OPTIONS) as cfg:
+    with create_env(cfg) as env:
+
+        make_all_vars(env, DECLS)
+        assert_string_formulas(env, HARD)
+        assert_string_soft_formulas_dict(env, SOFT)
+
+        with create_maximize(env, "cost") as obj1, \
+             create_minimize(env, "cost") as obj2, \
+             create_minmax(env, ["x1", "x2"]) as obj3:
+
+            push(env)
+            assert_objective(env, obj1)
+            solve(env)
+            dump_model(env)
+            pop(env)
+
+            push(env)
+            assert_objective(env, obj2)
+            solve(env)
+            dump_model(env)
+            pop(env)
+
+            push(env)
+            assert_objective(env, obj3)
+            assert_string_formula(env, "(or (<= x1 7) (<= x1 3))")
+            solve(env)
+            dump_model(env)
+            pop(env)
+
+#
+## EXPECTED OUTPUT
+#
+# sat
+#   B1 : `true`
+#   x1 : 9
+#   x2 : 10
+#   cost : 19
+# sat
+#   B1 : `true`
+#   x1 : 0
+#   x2 : 1
+#   cost : 1
+# sat
+#   B1 : `true`
+#   x1 : 7
+#   x2 : 10
+#   cost : 17
+#   minmax : 7
