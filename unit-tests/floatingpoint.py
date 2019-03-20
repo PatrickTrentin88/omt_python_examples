c103c658edb13859b5f17406b4406cccaa476f6c
PatrickTrentin88 <patrick.trentin.88@gmail.com>
2019-03-20 15:27:29 +0100

    floatingpoint.py

---
+++ unit-tests/floatingpoint.py
@@ -0,0 +1,85 @@
+"""
+Floating-Point unit-test.
+"""
+
+from wrapper import * # pylint: disable=unused-wildcard-import,wildcard-import
+
+###
+### DATA
+###
+
+OPTIONS = {
+    "opt.priority"     : "box",
+    "model_generation" : "true",
+}
+
+DECLS = {
+    "bool" : (),        # (name, ...)
+    "int"  : (),        # (name, ...)
+    "rational" : (),    # (name, ...)
+    "bv" : (),          # ((name, width), ... )
+    "fp" : (            # ((name, ebits, sbits), ... )
+        ("x0", 8, 23),
+        ("x1", 8, 23),
+        ("x2", 8, 23),
+        ("x3", 8, 23),
+    )
+}
+
+_M_INF = "(fp #b1 #b11111111 #b00000000000000000000000)"
+_M_TEN = "(fp #b1 #b10000010 #b01000000000000000000000)"
+_ZERO = "(fp #b0 #b00000000 #b00000000000000000000000)"
+_P_TEN = "(fp #b0 #b10000010 #b01000000000000000000000)"
+_P_INF = "(fp #b0 #b11111111 #b00000000000000000000000)"
+
+HARD = (
+    "(fp.leq {} x0)".format(_M_TEN),
+    "(fp.leq x0 {})".format(_P_TEN),
+    "(= x1 {})".format(_ZERO),
+    "(= x2 (_ NaN 8 24))",
+    "(fp.leq {} x3)".format(_M_INF),
+    "(fp.leq x3 {})".format(_P_INF),
+)
+
+SOFT = {}
+
+###
+### FLOATING-POINT UNIT-TEST
+###
+
+with create_config(OPTIONS) as cfg:
+    with create_env(cfg) as env:
+
+        make_all_vars(env, DECLS)
+        assert_string_formulas(env, HARD)
+        assert_string_soft_formulas_dict(env, SOFT)
+
+        with create_minimize(env, "x0") as obj1, \
+             create_maximize(env, "x0") as obj2, \
+             create_minimize(env, "x1") as obj3, \
+             create_minimize(env, "x2") as obj4, \
+             create_minimize(env, "x3", lower=_M_TEN, upper=_P_TEN) as obj5, \
+             create_maximize(env, "x3", lower=_M_TEN, upper=_P_TEN) as obj6:
+
+            assert_objective(env, obj1)
+            assert_objective(env, obj2)
+            assert_objective(env, obj3)
+            assert_objective(env, obj4)
+            assert_objective(env, obj5)
+            assert_objective(env, obj6)
+
+            solve(env)
+            get_objectives_pretty(env)
+
+#
+## EXPECTED OUTPUT
+#
+# sat
+# (objectives
+#   (x0 3240099840_8_23)
+#   (x0 1092616192_8_23)
+#   (x1 0_8_23)
+#   (x2 4286578689_8_23)
+#   (x3 3240099840_8_23)
+#   (x3 1092616192_8_23)
+# )
